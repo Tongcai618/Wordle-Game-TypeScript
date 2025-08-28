d7299e9a2cfe870829093a704fa4daa3a4cf66ff
Tong Cai <tongcai010618@gmail.com>
2025-08-28 14:53:19 -0400

    Replace the backend server url with the aws public ip address.

--- wordle-typescript/src/apis/http.tsx
+++ wordle-typescript/src/apis/http.tsx
@@ -1,7 +1,7 @@
 // src/apis/http.ts
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8081/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://18.218.12.164:8081/api";
 
 const http = axios.create({
   baseURL: BASE_URL,
